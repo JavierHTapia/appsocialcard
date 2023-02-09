75d17ad487df220248d52e4e60181ab683912e19
JavierHTapia <javier.huanca.tapia@gmail.com>
2023-02-09 01:08:56 -0400

    primer commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+/* theme script */
+const switcher = document.querySelector('#theme-options')
+const doc = document.firstElementChild
+
+switcher.addEventListener('input', e =>
+  setTheme(e.target.value))
+
+const setTheme = theme =>
+  doc.setAttribute('color-scheme', theme)
+/* animation script */
+const showSocial = (toggleCard, socialCard)=>{
+    const toggle = document.getElementById(toggleCard),
+            social = document.getElementById(socialCard)
+    toggle.addEventListener('click',()=>{
+        if(social.classList.contains('animation')){
+            social.classList.add('down-animation')
+            setTimeout(()=>{
+                social.classList.remove('down-animation')
+            },1000)
+        }
+        social.classList.toggle('animation')
+    })
+}
+showSocial('card-toggle','card-social')
